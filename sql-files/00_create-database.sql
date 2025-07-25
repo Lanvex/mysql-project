c964b56bbfdf53a4e5001e5f8ae57f691b27a7fd
Maximilian Huag <maximilian@haug-it.eu>
2025-07-25 09:37:45 +0200

    create database file

---
+++ sql-files/00_create-database.sql
@@ -0,0 +1,6 @@
+-- Database Project - Online Game Store - Maximilian Haug, Niklas Busch, Marc Marjan
+
+-- Check if the database exists, drop it if it does, and create a new one
+DROP DATABASE IF EXISTS it11d_gruppe9_videogame_store;
+
+CREATE DATABASE it11d_gruppe9_videogame_store;
